70d84ce7902dca170c7fd977379154b729748a1a
Sajank203 <SAJANPREET1712003@ICLOUD.COM>
2024-08-15 13:05:02 +1200

    Add files via upload
    ACTIVITES OF WEEK 4

---
+++ ex 1.py
@@ -0,0 +1,10 @@
+#Author : Sana Alyaseri
+#Class software development
+def greet(name):
+    print(f"Hello, {name}!")
+    
+    #call the function with a parameter 
+    greet("Alice")
+    greet("Bob")
+
+    
